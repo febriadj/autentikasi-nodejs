7a617fcd8ec2db9ee25a17cbc8a3806204a743e9
i'mfebri <iamfebriadji@gmail.com>
2021-05-03 22:58:08 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 "use strict"
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const session = require('express-session');
-const app = express();
+const 
+  express = require('express')
+, path = require('path')
+, session = require('express-session')
+, app = express()
+, port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// konfigurasi static file
 app.use(express.static(path.join(__dirname, 'public/css')));
 
 app.use(session({
   secret: 'secret',
-  resave: true,
+  resave: false,
   saveUninitialized: true
 }));
 
 app.use(require('./router/route'));
 
-const port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log(`listening on localhost:${port}`);
 });
