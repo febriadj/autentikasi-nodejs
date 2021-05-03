21ab8deb08c234b780a875e446bf99fd9f7f166c
i'mfebri <iamfebriadji@gmail.com>
2021-05-04 01:35:47 +0700

    menambahkan router tampilan awal & logout pengguna

--- router/route.js
+++ router/route.js
@@ -1,15 +1,17 @@
 'use strict'
 
-const
-  router = require('express').Router()
-, bcrypt = require('bcryptjs');
+const router = require('express').Router()
 
-// router login
-const { loginGet, loginPost } = require('../controllers/login');
-router.route('/login').get(loginGet).post(loginPost);
+// router login dan initial view
+const { loginPost } = require('../controllers/login');
+const { initialView } = require('../controllers/initial_view')
+router.route('/').get(initialView).post(loginPost);
 
 // router register
 const { registerGet, registerPost } = require('../controllers/register');
 router.route('/register').get(registerGet).post(registerPost);
+
+const { logout } = require('../controllers/logout');
+router.get('/logout', logout);
 
 module.exports = router;
