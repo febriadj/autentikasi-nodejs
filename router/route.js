930687521de290aaf56e018867f0c216d0c1130a
i'mfebri <iamfebriadji@gmail.com>
2021-05-04 00:32:27 +0700

    menambahkan router register

--- router/route.js
+++ router/route.js
@@ -2,10 +2,14 @@
 
 const
   router = require('express').Router()
-, bcrypt = require('bcryptjs')
+, bcrypt = require('bcryptjs');
 
 // router login
-const { loginGet, loginPost } = require('./login')
-router.route('/login').get(loginGet).post(loginPost)
+const { loginGet, loginPost } = require('../controllers/login');
+router.route('/login').get(loginGet).post(loginPost);
+
+// router register
+const { registerGet, registerPost } = require('../controllers/register');
+router.route('/register').get(registerGet).post(registerPost);
 
 module.exports = router;
